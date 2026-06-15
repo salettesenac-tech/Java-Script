591a198442773b895eb21ef092098022ca141b1e
salettesenac-tech <salettesenac@gmail.com>
2026-06-15 16:37:47 -0300

    Adicionado operadores de Atribuição

---
+++ op-comparacao-de-igualdade-estrita-arrayss.js
@@ -0,0 +1,10 @@
+// ListaA é um array com 5 elementos
+let listaA = [10, 20, 30, 40, 50];
+
+// ListaB é outro array com os mesmos 5 elementos que listaA
+let listaB = [10, 20, 30 ,40, 50];
+
+// ListaC recebe uma copia da listaA
+let listaC = listaA;
+
+console.log(listaA == listaC);
